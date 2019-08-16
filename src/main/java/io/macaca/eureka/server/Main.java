1b811f1d343855a3583db1d2e5928b082412310c
Mayowa Aj <sagaraaj69@gmail.com>
2019-08-16 09:20:37 +0100

    clients can be registered on the eureka server

--- src/main/java/io/macaca/eureka/server/Main.java
+++ src/main/java/io/macaca/eureka/server/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer    // Enable eureka server
+@EnableEurekaServer
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
